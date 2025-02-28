5bcdcd6945b51aad808587af2ab00b7e0ea63a86
Misael Fernández <misael.fernandez@mintel.gob.ec>
2025-02-28 15:48:43 -0500

    V4.0.0-BETA-6 - Se se corrige bug para vulnerabilidad RC

--- src/main/java/ec/gob/firmadigital/libreria/utils/CheckPDF.java
+++ src/main/java/ec/gob/firmadigital/libreria/utils/CheckPDF.java
@@ -74,69 +74,92 @@ public class CheckPDF {
         }
     }
 
+    private static String getPdfObject(PdfObject pdfObject) {
+        StringBuilder stringBuilder = new StringBuilder();
+        if (pdfObject != null) {
+            if (pdfObject.isDictionary()) {
+                getJavaScriptFromPdfDictionary((PdfDictionary) pdfObject, stringBuilder);
+            } else if (pdfObject.isArray()) {
+                getJavaScriptFromPdfArray((PdfArray) pdfObject, stringBuilder);
+            } else if (pdfObject.isString()) {
+                stringBuilder.append(((PdfString) pdfObject).getValue());
+            } else if (pdfObject.isName()) {
+                stringBuilder.append(((PdfName) pdfObject).getValue());
+            } else {
+                stringBuilder.append(pdfObject);
+            }
+        }
+        return stringBuilder.toString().trim();
+    }
+
     public static String checkPDF(PdfDocument pdfDocument) {
         checkPDF = "";
-
         //To get javascript that is added through OpenAction
         PdfDictionary pdfDictionaryCatalog = pdfDocument.getCatalog().getPdfObject();
         PdfDictionary namesDictionary = pdfDictionaryCatalog.getAsDictionary(PdfName.OpenAction);
         if (namesDictionary != null && !namesDictionary.isEmpty()) {
+            String openAction = "";
             PdfObject pdfObject = namesDictionary.get(PdfName.JS);
             if (pdfObject != null) {
-                StringBuilder strBuf = new StringBuilder();
-                if (pdfObject.isDictionary()) {
-                    getJavaScriptFromPdfDictionary((PdfDictionary) pdfObject, strBuf);
-                } else if (pdfObject.isArray()) {
-                    getJavaScriptFromPdfArray((PdfArray) pdfObject, strBuf);
-                } else if (pdfObject.isString()) {
-                    strBuf.append(((PdfString) pdfObject).getValue());
-                }
-                checkPDF += "OPENACTION:\n" + strBuf.toString().trim();
+                openAction += "\n" + PdfName.JS.getValue() + ": " + getPdfObject(pdfObject);
+            }
+            pdfObject = namesDictionary.get(PdfName.URI);
+            if (pdfObject != null) {
+                openAction += "\n" + PdfName.URI.getValue() + ": " + getPdfObject(pdfObject);
+            }
+            pdfObject = namesDictionary.get(PdfName.S);
+            if (pdfObject != null) {
+                openAction += "\n" + PdfName.S.getValue() + ": " + getPdfObject(pdfObject);
+            }
+            pdfObject = namesDictionary.get(PdfName.EmbeddedFile);
+            if (pdfObject != null) {
+                openAction += "\n" + PdfName.EmbeddedFile.getValue() + ": " + getPdfObject(pdfObject);
+            }
+            pdfObject = namesDictionary.get(PdfName.EmbeddedFiles);
+            if (pdfObject != null) {
+                openAction += "\n" + PdfName.EmbeddedFiles.getValue() + ": " + getPdfObject(pdfObject);
+            }
+            if (!openAction.isEmpty()) {
+                checkPDF = "OpenAction:" + openAction;
             }
         }
-
         // To get java script available from NAMES dictionary 
         namesDictionary = pdfDictionaryCatalog.getAsDictionary(PdfName.Names);
         if (namesDictionary != null && !namesDictionary.isEmpty()) {
+            String javaScript = "";
             PdfDictionary javascriptDictionary = namesDictionary.getAsDictionary(PdfName.JavaScript);
             if (javascriptDictionary != null) {
                 StringBuilder stringBuilder = new StringBuilder();
                 Set<Map.Entry<PdfName, PdfObject>> set = javascriptDictionary.entrySet();
                 for (Map.Entry<PdfName, PdfObject> entry : set) {
-                    PdfObject pdfObj = entry.getValue();
-                    if (pdfObj.isDictionary()) {
-                        getJavaScriptFromPdfDictionary((PdfDictionary) pdfObj, stringBuilder);
-                    } else if (pdfObj.isArray()) {
-                        getJavaScriptFromPdfArray((PdfArray) pdfObj, stringBuilder);
-                    } else if (pdfObj.isString() && entry.getKey().getValue().equals(PdfName.JS.getValue())) {
-                        stringBuilder.append(((PdfString) pdfObj).getValue());
-                    }
+                    PdfObject pdfObject = entry.getValue();
+                    javaScript += "\n" + getPdfObject(pdfObject);
                 }
-                checkPDF += "\nJAVASCRIPT:\n" + stringBuilder.toString().trim();
+                checkPDF += "\n" + stringBuilder.toString().trim();
+            }
+            if (!javaScript.isEmpty()) {
+                checkPDF += "Names Dictionary - JavaScript:" + javaScript;
             }
         }
-
         // To get java script from name tree JAVASCRIPT
         PdfNameTree nameTree = pdfDocument.getCatalog().getNameTree(PdfName.JavaScript);
         if (nameTree != null && !nameTree.getNames().isEmpty()) {
+            String javaScript = "";
             Map<String, PdfObject> objs = nameTree.getNames();
             if (objs != null) {
                 StringBuilder stringBuilder = new StringBuilder();
                 for (Map.Entry<String, PdfObject> entry : objs.entrySet()) {
                     PdfObject pdfObject = entry.getValue();
-                    if (pdfObject.isDictionary()) {
-                        getJavaScriptFromPdfDictionary((PdfDictionary) pdfObject, stringBuilder);
-                    } else if (pdfObject.isArray()) {
-                        getJavaScriptFromPdfArray((PdfArray) pdfObject, stringBuilder);
-                    } else if (pdfObject.isString() && entry.getKey().equals(PdfName.JS.getValue())) {
-                        stringBuilder.append(((PdfString) pdfObject).getValue());
-                    }
+                    javaScript += "\n" + getPdfObject(pdfObject);
                 }
-                checkPDF += "\nJAVASCRIPT NAMED TREE:\n" + stringBuilder.toString().trim();
+                checkPDF += "\n" + stringBuilder.toString().trim();
+            }
+            if (!javaScript.isEmpty()) {
+                checkPDF += "Name Tree - JavaScript" + javaScript;
             }
         }
-
         // To get java script at the annotation action level
+        String javaScriptAnnotation = "";
         for (int i = 1; i <= pdfDocument.getNumberOfPages(); i++) {
             PdfPage page = pdfDocument.getPage(i);
             List<PdfAnnotation> annotList = page.getAnnotations();
@@ -147,19 +170,20 @@ public class CheckPDF {
                         if (annotationAction != null && PdfName.JavaScript.equals(annotationAction.get(PdfName.S))) {
                             PdfString javascript = annotationAction.getAsString(PdfName.JS);
                             if (javascript != null) {
-                                checkPDF += javascript;
+                                checkPDF += "\n" + javascript;
                             }
                         }
                     }
                 }
             }
         }
-
+        if (!javaScriptAnnotation.isEmpty()) {
+            checkPDF += "Annotation Action Level - JavaScript" + javaScriptAnnotation;
+        }
         if (!checkPDF.isEmpty()) {
             LOGGER.log(Level.WARNING, checkPDF.trim());
             checkPDF = "El documento es potencialmente sospechoso para procesarlo";
         }
-
 //        PdfDictionary rootDictionary = pdfDocument.getTrailer().getAsDictionary(PdfName.Root).getAsDictionary(PdfName.OpenAction);
 //        if (rootDictionary != null) {
 //            rootDictionary.entrySet().forEach(entry -> {
